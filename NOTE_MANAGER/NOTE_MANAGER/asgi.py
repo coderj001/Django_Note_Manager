2fab3afead3643a7a7b207877b1c15c29df03350
mr-zero <amirajubolchi001@gmail.com>
2020-08-04 19:12:03 +0530

    Django version change

--- NOTE_MANAGER/NOTE_MANAGER/asgi.py
+++ NOTE_MANAGER/NOTE_MANAGER/asgi.py
@@ -1,16 +1,16 @@
 """
-ASGI config for NOTE_MANGER project.
+ASGI config for NOTE_MANAGER project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
+https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
 """
 
 import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'NOTE_MANGER.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'NOTE_MANAGER.settings')
 
 application = get_asgi_application()
